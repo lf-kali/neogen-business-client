6a533ed8e00af805c713019b1d0a3866a0153354
lf-kali <cbjk.corp@gmail.com>
2026-01-20 16:21:55 -0300

    feat: adicionado cadastro de cliente

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import TechRegister from './pages/tech-register/TechRegister'
 import Login from './pages/login/Login'
+import CustomerRegister from './pages/customer-register/CustomerRegister'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
             <Route path='/' element={<Login/>}/>
             <Route path='/login' element={<Login/>}/>
             <Route path='/technician/register' element={<TechRegister/>}/>
+            <Route path='/customers/register' element={<CustomerRegister/>}/>
           </Routes>
         </div>
       </BrowserRouter>
---
+++ src/pages/tech-login/TechLogin.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TechLogin() {
+  return (
+    <>
+        
+    </>
+  )
+}
+
+export default TechLogin
